6c9b241ae7c4b4038f63dfb902911ac56908ab07
jjohn92085 <jjohn92085@gmail.com>
2025-09-10 00:36:57 -0600

    created function to create divs

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//creates multiple divs
 function createDivs() {
     const containerDiv = document.getElementById('mainDiv');
 
     for (let i = 0; i <= 16; i++) {
-        const newDivs = document.createElement('div');
+        const newDivs = document.createElement('div')
+        newDivs.innerText= 'testdivs'
+        containerDiv.appendChild(newDivs)
     }
-
-    
 }
+
+window.addEventListener('DOMContentLoaded', createDivs)

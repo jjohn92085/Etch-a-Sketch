348774a46d7c9926bddf0fdce4b2c84816d5da4f
jjohn92085 <jjohn92085@gmail.com>
2025-09-10 00:32:26 -0600

    etch-a-sketch project

---
+++ script.js
@@ -0,0 +1,9 @@
+function createDivs() {
+    const containerDiv = document.getElementById('mainDiv');
+
+    for (let i = 0; i <= 16; i++) {
+        const newDivs = document.createElement('div');
+    }
+
+    
+}
